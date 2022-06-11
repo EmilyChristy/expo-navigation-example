6ea6dd988f33b949984ba8abbe4cde9943dc9e4c
Emily Christy <echristy@caci.co.uk>
2022-06-11 12:36:02 +0100

    Added basic two screens and stacknavigator

--- App.js
+++ App.js
@@ -1,11 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { StyleSheet, Text, Button, View } from "react-native";
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Details" component={Details} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+function Home({ navigation }) {
+  return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Home Screen</Text>
+      <Button
+        title="Show me the Details screen"
+        onPress={() => navigation.navigate("Details")}
+      />
+    </View>
+  );
+}
+
+function Details({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Text>This is the Details page</Text>
+      <Button
+        title="Back to the Home Screen"
+        onPress={() => navigation.navigate("Home")}
+      />
     </View>
   );
 }
@@ -13,8 +43,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
