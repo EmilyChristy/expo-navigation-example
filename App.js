ba898ddb7713c0de9af0cd2eb07bbf3251f45a81
Emily Christy <echristy@caci.co.uk>
2022-06-11 14:00:40 +0100

    Added search screen

--- App.js
+++ App.js
@@ -1,17 +1,20 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { StyleSheet, Text, Button, View } from "react-native";
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="Details" component={Details} />
-      </Stack.Navigator>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={Home} />
+        <Tab.Screen name="Details" component={Details} />
+        <Tab.Screen name="Search" component={Search} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
@@ -32,6 +35,18 @@ function Details({ navigation }) {
   return (
     <View style={styles.container}>
       <Text>This is the Details page</Text>
+      <Button
+        title="Back to the Home Screen"
+        onPress={() => navigation.navigate("Home")}
+      />
+    </View>
+  );
+}
+
+function Search({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Text>This is the Search page</Text>
       <Button
         title="Back to the Home Screen"
         onPress={() => navigation.navigate("Home")}
